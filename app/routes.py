27635ee91b217a72cc97ee89945f56a3b58a6988
Younes <boukroun.younes@gmail.com>
2019-01-22 14:16:22 +0100

    Updates made to push and deploy the application to heroku server

--- app/routes.py
+++ app/routes.py
@@ -5,5 +5,7 @@ app = Flask(__name__)
 
 
 
+
+
 if __name__ == "__main__":
     app.run(debug=True)
--- tests/testing_app.py
+++ tests/testing_app.py
@@ -1,12 +1,12 @@
 import unittest
-import app.routes
-from app.routes import home
+
+from app import app
 
 class Mytdd(unittest.TestCase):
     """This class made to make all our tests for the project"""
     
     def test_vue_runing(self):
-        pass
+        
 
     
 
