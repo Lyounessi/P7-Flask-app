9ee059d5d9e13eb8c6d23c1d527abf60acd18601
Younes <boukroun.younes@gmail.com>
2019-01-22 01:50:32 +0100

    Creat first test of the vue in the red mode

---
+++ tests/testing_app.py
@@ -0,0 +1,15 @@
+import unittest
+import app.routes
+from app.routes import home
+
+class Mytdd(unittest.TestCase):
+    """This class made to make all our tests for the project"""
+    
+    def test_vue_runing(self):
+        pass
+
+    
+
+    
+if __name__ == '__main__':
+    unittest.main()
