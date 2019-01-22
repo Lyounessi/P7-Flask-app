e2aa976311abf5d3b94eb98673865acb32ba9da0
Younes <boukroun.younes@gmail.com>
2019-01-22 19:01:27 +0100

    updates routes and tests

--- tests/testing_app.py
+++ tests/testing_app.py
@@ -4,10 +4,14 @@ from app import app
 
 class Mytdd(unittest.TestCase):
     """This class made to make all our tests for the project"""
-    
-    def test_vue_runing(self):
+    def setUp(self):
+        app.testing = True
+        self.app = app.test_client()
         
-
+        
+    def test_vue_runing(self):
+        result = self.app.get("/")
+        assert result.status_code == 200
     
 
     
